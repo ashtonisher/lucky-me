5ae99143b6eb92ed04896861f08405f473672602
khchoi <khchoi@banaple.co.kr>
2026-03-06 08:24:18 +0900

    [mod] node 버전 명시, README 업데이트

--- public/js/ngrok-skip.js
+++ public/js/ngrok-skip.js
@@ -1,11 +1,12 @@
-
 /** ngrok warning page remove ajax */
+const url = window.location.origin; // 현재 호스트 기준으로 요청
+
 fetch(url, {
-    method: "GET",
-    headers: new Headers({
-      "ngrok-skip-browser-warning": "950130",
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+  method: "GET",
+  headers: new Headers({
+    "ngrok-skip-browser-warning": "950130",
+  }),
+})
+  .then((response) => response.text())
+  .then(() => console.log("ngrok skip header sent"))
+  .catch((err) => console.log(err));
